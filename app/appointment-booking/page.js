4a5fd1964cb3f0463e7549982d0f56acfa0713c5
Nawwar <nawwar.d92@gmail.com>
2024-11-26 20:17:55 +0100

    created appointment form, appointment booking page, added styles

--- app/appointment-booking/page.js
+++ app/appointment-booking/page.js
@@ -1,3 +1,21 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import AppointmentForm from "@/components/AppointmentForm";
+import styles from "./AppointmentBooking.module.css"; // Import the CSS module
+
 export default function AppointmentBooking() {
-  return <div>AppointmentBooking</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const doctorId = searchParams.get("doctorId");
+
+  if (!doctorId) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <div className={styles.appointmentBookingPage}>
+      <h1 className={styles.pageHeading}>Appointment Booking</h1>
+      <AppointmentForm doctorId={doctorId} />
+    </div>
+  );
 }
